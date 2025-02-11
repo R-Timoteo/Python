c2a15d37cb85e7a19639dfb5e482e83606881270
RTimoteo <rafael.tmtrodrigues@gmail.com>
2025-02-10 21:59:59 -0300

    Primeiro commit

---
+++ expython1.py
@@ -0,0 +1,4 @@
+nome = "Timoteo"
+idade = 28
+
+print (nome, idade)
---
+++ inoutput.py
@@ -0,0 +1,9 @@
+nome = "rafael"
+sobrenome = "timoteo"
+
+
+print (nome, sobrenome)
+
+print (nome, sobrenome, sep= "#")
+
+print (nome, sobrenome, end= "...\n")
